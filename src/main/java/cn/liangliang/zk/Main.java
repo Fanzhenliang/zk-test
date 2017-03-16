e7b25a6d70c45caa937b7fe22eee3f930ef76a3d
liangliang <779699378@qq.com>
2017-03-16 11:05:32 +0800

    modify Main.java, MainTest.java

--- src/main/java/cn/liangliang/zk/Main.java
+++ src/main/java/cn/liangliang/zk/Main.java
@@ -3,6 +3,7 @@ package cn.liangliang.zk;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
+		System.out.println("This is the new branch1 modify");
 	}
 
 }
