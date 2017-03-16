eece2f2d351fd1273c9af1712c813ec5432a1eb6
liangliang <779699378@qq.com>
2017-03-16 10:31:47 +0800

    Main.java. MainTest.java pom.xml 3 files.

---
+++ src/main/java/cn/liangliang/zk/Main.java
@@ -0,0 +1,8 @@
+package cn.liangliang.zk;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+
+}
---
+++ src/test/java/cn/liangliang/zk/test/MainTest.java
@@ -0,0 +1,9 @@
+package cn.liangliang.zk.test;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Test HelloWorld!");
+	}
+	
+}
